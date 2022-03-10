4696cfe7a3c5b4374c6e189748e77978f6c8940e
mehedih23 <nmehedi23@gmail.com>
2022-03-10 21:10:38 +0600

    Fifth Debug Done

--- js/app.js
+++ js/app.js
@@ -149,9 +149,11 @@ const displayLikedPosts = () => {
   });
 };
 
-const displayReportedPosts = () => {
+const displayReportedPosts = () => { // Fifth Debug Done //
+  document.getElementById("reported").innerHTML = '';
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
